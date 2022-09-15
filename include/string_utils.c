60658ab3dedd42c43a4417984b7b03d9b58c8dfa
thiago-figueredo <thiago16fca@outlook.com>
2022-09-15 14:13:56 -0300

    add str_to_upper implementation

--- include/string_utils.c
+++ include/string_utils.c
@@ -1,8 +1,21 @@
-#include <string.h>
-#include "string.h"
 #include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "string.h"
 
 bool str_equals(char *s1, char *s2)
 {
     return strcmp(s1, s2) == 0;
+}
+
+char* str_to_upper(char* s)
+{
+    size_t length = strlen(s);
+
+    for (size_t i = 0; i < length; i++) {
+        s[i] = toupper(s[i]);
+    }
+
+    return s;
 }
